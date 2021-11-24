789e2daf58d70f076d19b2034c9acd0557f7fce8
Michael Wheeler <mpwheel@umich.edu>
2021-11-24 15:34:15 -0500

    Add import sorting with isort

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,13 @@ import nox
 
 
 @nox.session
+def sort(session):
+    """Checks that imports are correctly sorted using isort."""
+    session.install("-r", "requirements.txt")
+    session.run("isort", ".", "--check")
+
+
+@nox.session
 def format(session):
     """Checks that code is correctly formatted using black."""
     session.install("-r", "requirements.txt")
