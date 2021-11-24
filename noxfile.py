7f10fb5c2851f7506ba2e71624c09902ee070930
Michael Wheeler <mpwheel@umich.edu>
2021-11-24 15:44:33 -0500

    Add linting

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,13 @@ import nox
 
 
 @nox.session
+def lint(session):
+    """Checks for linting errors with flake8."""
+    session.install("-r", "requirements.txt")
+    session.run("flake8")
+
+
+@nox.session
 def sort(session):
     """Checks that imports are correctly sorted using isort."""
     session.install("-r", "requirements.txt")
