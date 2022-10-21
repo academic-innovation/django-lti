7397eb791952a95d283eb0b7cc09d8ac5d6a28cf
Michael Wheeler <mpwheel@umich.edu>
2022-10-21 12:02:23 -0400

    Store and sync AGS claim (#31)

--- lti_tool/constants.py
+++ lti_tool/constants.py
@@ -21,3 +21,10 @@ class ContextRole(str, Enum):
     def full_name(self) -> str:
         """Return the full name of this roles."""
         return self.value
+
+
+class AgsScope(str, Enum):
+    MANAGE_LINEITEMS = "https://purl.imsglobal.org/spec/lti-ags/scope/lineitem"
+    QUERY_LINEITEMS = "https://purl.imsglobal.org/spec/lti-ags/scope/lineitem.readonly"
+    PUBLISH_SCORES = "https://purl.imsglobal.org/spec/lti-ags/scope/score"
+    ACCESS_RESULTS = "https://purl.imsglobal.org/spec/lti-ags/scope/result.readonly"
