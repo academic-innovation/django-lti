7d75c61ac0fa1bdb7b0c4cd3e7a3185ac49760fe
Colin Murtaugh <cmurtaugh@gmail.com>
2023-03-02 09:06:40 -0500

    added ContextType constant

--- lti_tool/constants.py
+++ lti_tool/constants.py
@@ -4,6 +4,8 @@ SESSION_KEY = "_lti_tool_launch_id"
 
 CONTEXT_ROLE_PATTERN = "http://purl.imsglobal.org/vocab/lis/v2/membership#{}"
 
+CONTEXT_TYPE_PATTERN = "http://purl.imsglobal.org/vocab/lis/v2/course#{}"
+
 
 class ContextRole(str, Enum):
     ADMINISTRATOR = CONTEXT_ROLE_PATTERN.format("Administrator")
@@ -19,5 +21,22 @@ class ContextRole(str, Enum):
 
     @property
     def full_name(self) -> str:
-        """Return the full name of this roles."""
+        """Return the full name of this role."""
+        return self.value
+
+
+class ContextType(str, Enum):
+    COURSE_TEMPLATE = CONTEXT_TYPE_PATTERN.format("CourseTemplate")
+    COURSE_OFFERING = CONTEXT_TYPE_PATTERN.format("CourseOffering")
+    COURSE_SECTION = CONTEXT_TYPE_PATTERN.format("CourseSection")
+    GROUP = CONTEXT_TYPE_PATTERN.format("Group")
+
+    @property
+    def short_name(self) -> str:
+        """"Return the short name of this context."""
+        return self.value[len(CONTEXT_TYPE_PATTERN.format("")) :]
+
+    @property
+    def full_name(self) -> str:
+        """Return the full name of this context."""
         return self.value
