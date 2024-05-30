3e06b52b1f91f896561b56988ffdab4e53bf8556
Michael Wheeler <mpwheel@umich.edu>
2024-05-30 12:21:23 -0400

    Move registration key fields to fieldset in admin form (#105)

--- lti_tool/admin.py
+++ lti_tool/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext as _
 
 from . import models
 
@@ -9,4 +10,29 @@ class LtiDeploymentInline(admin.TabularInline):
 
 @admin.register(models.LtiRegistration)
 class LtiRegistrationAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    "name",
+                    "uuid",
+                    "issuer",
+                    "client_id",
+                    "auth_url",
+                    "token_url",
+                    "keyset_url",
+                    "is_active",
+                ]
+            },
+        ),
+        (
+            _("Static keys"),
+            {
+                "description": _("For platforms not supporting JWK Set use by tools."),
+                "fields": ["public_key", "private_key"],
+                "classes": ["collapse"],
+            },
+        ),
+    ]
     inlines = [LtiDeploymentInline]
