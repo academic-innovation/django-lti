585c8708c3e3d9c2491f2183b71f92f28aba8f4a
Colin Murtaugh <cmurtaugh@gmail.com>
2025-03-05 11:58:14 -0500

    Rough first pass at an authentication backend and middleware, based on RemoteUserBackend and RemoteUserMiddleware

---
+++ lti_tool/auth/backends.py
@@ -0,0 +1,113 @@
+from logging import getLogger
+
+from asgiref.sync import sync_to_async
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+from lti_tool.types import LtiLaunch
+
+logger = getLogger(__name__)
+
+UserModel = get_user_model()
+
+
+class LtiLaunchAuthenticationBackend(ModelBackend):
+    """
+    This backend is to be used in conjunction with the ``LtiLaunchAuthenticationMiddleware``
+    found in the middleware module of this package, and is used when authentication is
+    is handled by an LMS and passed in via LTI launch.
+
+    By default, the ``authenticate`` method creates ``User`` objects for
+    usernames that don't already exist in the database.  Subclasses can disable
+    this behavior by setting the ``create_unknown_user`` attribute to
+    ``False``.
+    """
+
+    # Create a User object if not already in the database?
+    create_unknown_user = True
+
+    def authenticate(self, request, lti_launch_user_id):
+        """
+        The username passed as ``lti_launch_user_id`` is considered trusted. Return
+        the ``User`` object with the given username. Create a new ``User``
+        object if ``create_unknown_user`` is ``True``.
+
+        Return None if ``create_unknown_user`` is ``False`` and a ``User``
+        object with the given username is not found in the database.
+        """
+        if not lti_launch_user_id:
+            return
+        created = False
+        user = None
+        username = self.clean_username(lti_launch_user_id)
+
+        # Note that this could be accomplished in one try-except clause, but
+        # instead we use get_or_create when creating unknown users since it has
+        # built-in safeguards for multiple threads.
+        if self.create_unknown_user:
+            user, created = UserModel._default_manager.get_or_create(
+                **{UserModel.USERNAME_FIELD: username}
+            )
+        else:
+            try:
+                user = UserModel._default_manager.get_by_natural_key(username)
+            except UserModel.DoesNotExist:
+                pass
+        user = self.configure_user(request, user, created=created)
+        return user if self.user_can_authenticate(user) else None
+
+    async def aauthenticate(self, request, lti_launch_user_id):
+        """See authenticate()."""
+        if not lti_launch_user_id:
+            return
+        created = False
+        user = None
+        username = self.clean_username(lti_launch_user_id)
+
+        # Note that this could be accomplished in one try-except clause, but
+        # instead we use get_or_create when creating unknown users since it has
+        # built-in safeguards for multiple threads.
+        if self.create_unknown_user:
+            user, created = await UserModel._default_manager.aget_or_create(
+                **{UserModel.USERNAME_FIELD: username}
+            )
+        else:
+            try:
+                user = await UserModel._default_manager.aget_by_natural_key(username)
+            except UserModel.DoesNotExist:
+                pass
+        user = await self.aconfigure_user(request, user, created=created)
+        return user if self.user_can_authenticate(user) else None
+
+    def clean_username(self, username):
+        """
+        Perform any cleaning on the "username" prior to using it to get or
+        create the user object.  Return the cleaned username.
+
+        By default, return the username unchanged.
+        """
+        return username
+
+    def configure_user(self, request, user, created=True):
+        """
+        Configure a user and return the updated user.
+
+        By default, return the user unmodified.
+        """
+        if created:
+            logger.info(f"User {user} created")
+            if hasattr(request, "lti_launch"):
+                launch: LtiLaunch = request.lti_launch
+                user.first_name = launch.user.given_name
+                user.last_name = launch.user.family_name
+                user.email = launch.user.email
+                user.save()
+        return user
+
+    async def aconfigure_user(self, request, user, created=True):
+        """See configure_user()"""
+        return await sync_to_async(self.configure_user)(request, user, created)
+
+
+class AllowAllUsersLtiLaunchAuthenticationBackend(LtiLaunchAuthenticationBackend):
+    def user_can_authenticate(self, user):
+        return True
---
+++ lti_tool/auth/middleware.py
@@ -0,0 +1,189 @@
+from logging import getLogger
+
+from asgiref.sync import iscoroutinefunction, markcoroutinefunction
+from django.contrib import auth
+from django.contrib.auth import load_backend
+from django.core.exceptions import ImproperlyConfigured
+
+from inspector.auth.backends import LtiLaunchAuthenticationBackend
+
+logger = getLogger(__name__)
+
+
+class LtiLaunchAuthenticationMiddleware:
+    """
+    Middleware for utilizing LMS-provided authentication via LTI launch.
+
+    This middleware works in conjunction with `LtiLaunchMiddleware`.  The `LtiLaunchMiddleware`
+    MUST appear before this middleware in the middleware list.
+
+    If request.user is not authenticated, then this middleware attempts to
+    authenticate the username from ``request.lti_launch.user.sub``.
+
+    If authentication is successful, the user is automatically logged in to
+    persist the user in the session.
+
+    """
+
+    sync_capable = True
+    async_capable = True
+
+    def __init__(self, get_response):
+        if get_response is None:
+            raise ValueError("get_response must be provided.")
+        self.get_response = get_response
+        self.is_async = iscoroutinefunction(get_response)
+        if self.is_async:
+            markcoroutinefunction(self)
+        super().__init__()
+
+    # Name of request.META key to grab username from. Note that for
+    # request headers, normalization to all uppercase and the addition
+    # of a "HTTP_" prefix apply.
+    header = "REMOTE_USER"
+    force_logout_if_no_header = True
+
+    def __call__(self, request):
+        if self.is_async:
+            return self.__acall__(request)
+        # AuthenticationMiddleware is required so that request.user exists.
+        if not hasattr(request, "lti_launch"):
+            raise ImproperlyConfigured(
+                "The Django LTI launch auth middleware requires the"
+                " LTI launch middleware to be installed.  Edit your"
+                " MIDDLEWARE setting to insert"
+                " 'lti_tool.auth.LtiLaunchMiddleware'"
+                " before the LtiLaunchAuthenticationMiddleware class."
+            )
+        try:
+            username = request.lti_launch.user.sub
+        except AttributeError:
+            # If the LTI launch user doesn't exist then remove any existing
+            # authenticated remote-user, or return (leaving request.user set to
+            # AnonymousUser by the AuthenticationMiddleware).
+            if self.force_logout_if_no_header and request.user.is_authenticated:
+                self._remove_invalid_user(request)
+            return self.get_response(request)
+        # If the user is already authenticated and that user is the user we are
+        # getting passed in the headers, then the correct user is already
+        # persisted in the session and we don't need to continue.
+        if request.user.is_authenticated:
+            if request.user.get_username() == self.clean_username(username, request):
+                return self.get_response(request)
+            else:
+                # An authenticated user is associated with the request, but
+                # it does not match the authorized user in the header.
+                self._remove_invalid_user(request)
+
+        # We are seeing this user for the first time in this session, attempt
+        # to authenticate the user.
+        user = auth.authenticate(request, lti_launch_user_id=username)
+        if user:
+            # User is valid.  Set request.user and persist user in the session
+            # by logging the user in.
+            request.user = user
+            auth.login(request, user)
+        return self.get_response(request)
+
+    async def __acall__(self, request):
+        # AuthenticationMiddleware is required so that request.user exists.
+        if not hasattr(request, "lti_launch"):
+            raise ImproperlyConfigured(
+                "The Django LTI launch auth middleware requires the"
+                " LTI launch middleware to be installed.  Edit your"
+                " MIDDLEWARE setting to insert"
+                " 'lti_tool.auth.LtiLaunchMiddleware'"
+                " before the LtiLaunchAuthenticationMiddleware class."
+            )
+        try:
+            username = request.lti_launch.user.sub
+        except AttributeError:
+            # If the LTI launch user doesn't exist then remove any existing
+            # authenticated remote-user, or return (leaving request.user set to
+            # AnonymousUser by the AuthenticationMiddleware).
+            if self.force_logout_if_no_header:
+                user = await request.auser()
+                if user.is_authenticated:
+                    await self._aremove_invalid_user(request)
+            return await self.get_response(request)
+        user = await request.auser()
+        # If the user is already authenticated and that user is the user we are
+        # getting passed in the headers, then the correct user is already
+        # persisted in the session and we don't need to continue.
+        if user.is_authenticated:
+            if user.get_username() == self.clean_username(username, request):
+                return await self.get_response(request)
+            else:
+                # An authenticated user is associated with the request, but
+                # it does not match the authorized user in the header.
+                await self._aremove_invalid_user(request)
+
+        # We are seeing this user for the first time in this session, attempt
+        # to authenticate the user.
+        user = await auth.aauthenticate(request, lti_launch_user_id=username)
+        if user:
+            # User is valid.  Set request.user and persist user in the session
+            # by logging the user in.
+            request.user = user
+            await auth.alogin(request, user)
+
+        return await self.get_response(request)
+
+    def clean_username(self, username, request):
+        """
+        Allow the backend to clean the username, if the backend defines a
+        clean_username method.
+        """
+        backend_str = request.session[auth.BACKEND_SESSION_KEY]
+        backend = auth.load_backend(backend_str)
+        try:
+            username = backend.clean_username(username)
+        except AttributeError:  # Backend has no clean_username method.
+            pass
+        return username
+
+    def _remove_invalid_user(self, request):
+        """
+        Remove the current authenticated user in the request which is invalid
+        but only if the user is authenticated via the LtiLaunchAuthenticationBackend.
+        """
+        try:
+            stored_backend = load_backend(
+                request.session.get(auth.BACKEND_SESSION_KEY, "")
+            )
+        except ImportError:
+            # backend failed to load
+            auth.logout(request)
+        else:
+            if isinstance(stored_backend, LtiLaunchAuthenticationBackend):
+                auth.logout(request)
+
+    async def _aremove_invalid_user(self, request):
+        """
+        Remove the current authenticated user in the request which is invalid
+        but only if the user is authenticated via the LtiLaunchAuthenticationBackend.
+        """
+        try:
+            stored_backend = load_backend(
+                await request.session.aget(auth.BACKEND_SESSION_KEY, "")
+            )
+        except ImportError:
+            # Backend failed to load.
+            await auth.alogout(request)
+        else:
+            if isinstance(stored_backend, LtiLaunchAuthenticationBackend):
+                await auth.alogout(request)
+
+
+class PersistentLtiLaunchAuthenticationMiddleware(LtiLaunchAuthenticationMiddleware):
+    """
+    Middleware for web-server provided authentication on logon pages.
+
+    Like LtiLaunchAuthenticationMiddleware but keeps the user authenticated even if
+    the ``request.META`` key is not found in the request. Useful for
+    setups when the external authentication is only expected to happen
+    on some "logon" URL and the rest of the application wants to use
+    Django's authentication mechanism.
+    """
+
+    force_logout_if_no_header = False
