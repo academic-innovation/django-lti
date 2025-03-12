ddc65d13f47006710c96ca5ea604f0269753a3f0
Colin Murtaugh <cmurtaugh@gmail.com>
2025-03-12 17:58:49 -0400

    fixed authenticate() param name; allow requests with no lti_launch to continue; also ensure that AuthenticationMiddleware is present

--- lti_tool/auth/middleware.py
+++ lti_tool/auth/middleware.py
@@ -4,6 +4,7 @@ from django.core.exceptions import ImproperlyConfigured
 from django.utils.deprecation import MiddlewareMixin
 
 from lti_tool.auth.backends import LtiLaunchAuthenticationBackend
+from lti_tool.models import LtiUser
 
 
 class LtiLaunchAuthenticationMiddleware(MiddlewareMixin):
@@ -26,6 +27,15 @@ class LtiLaunchAuthenticationMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
         # AuthenticationMiddleware is required so that request.user exists.
+        if not hasattr(request, "user"):
+            raise ImproperlyConfigured(
+                "The Django LTI launch auth middleware requires the"
+                " authentication middleware to be installed.  Edit your"
+                " MIDDLEWARE setting to insert"
+                " 'django.contrib.auth.middleware.AuthenticationMiddleware'"
+                " before the LtiLaunchAuthenticationMiddleware class."
+            )
+        # LtiLaunchMiddleware is required so that request.lti_launch exists.
         if not hasattr(request, "lti_launch"):
             raise ImproperlyConfigured(
                 "The Django LTI launch auth middleware requires the"
@@ -34,15 +44,23 @@ class LtiLaunchAuthenticationMiddleware(MiddlewareMixin):
                 " 'lti_tool.auth.LtiLaunchMiddleware'"
                 " before the LtiLaunchAuthenticationMiddleware class."
             )
+
+        # If the request doesn't have an LTILaunch object, then we can't
+        # authenticate the user.
+        if request.lti_launch.is_absent:  # pragma: no cover
+            if self.force_logout_if_no_launch and request.user.is_authenticated:
+                self._remove_invalid_user(request)
+            return
         try:
             username = request.lti_launch.user.sub
-        except AttributeError:
+        except LtiUser.DoesNotExist:
             # If the LTI launch user doesn't exist then remove any existing
             # authenticated remote-user, or return (leaving request.user set to
             # AnonymousUser by the AuthenticationMiddleware).
             if self.force_logout_if_no_launch and request.user.is_authenticated:
                 self._remove_invalid_user(request)
             return
+
         # If the user is already authenticated and that user is the user we are
         # getting passed in the LTI launch, then the correct user is already
         # persisted in the session and we don't need to continue.
@@ -56,7 +74,7 @@ class LtiLaunchAuthenticationMiddleware(MiddlewareMixin):
 
         # We are seeing this user for the first time in this session, attempt
         # to authenticate the user.
-        user = auth.authenticate(request, remote_user=username)
+        user = auth.authenticate(request, lti_launch_user_id=username)
         if user:
             # User is valid.  Set request.user and persist user in the session
             # by logging the user in.
