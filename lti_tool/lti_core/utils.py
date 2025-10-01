f1d5e82ab9788c74167cffeedb594348f390b06a
Michael Wheeler <mpwheel@umich.edu>
2025-10-01 13:03:34 -0400

    Add utility to validate the LTI 1.1 migration claim

--- lti_tool/lti_core/utils.py
+++ lti_tool/lti_core/utils.py
@@ -1,4 +1,6 @@
+import hmac
 import re
+from base64 import b64encode
 
 
 def normalize_role(role: str) -> str:
@@ -6,3 +8,30 @@ def normalize_role(role: str) -> str:
     if re.match(r"^\w+$", role):
         return f"http://purl.imsglobal.org/vocab/lis/v2/membership#{role}"
     return role
+
+
+def validate_migration_claim(launch_data: dict, oauth_secret: str) -> bool:
+    """Verifies a signature passed in the LTI 1.1 migration claim."""
+    try:
+        migration_claim = launch_data[
+            "https://purl.imsglobal.org/spec/lti/claim/lti1p1"
+        ]
+        oauth_consumer_key = migration_claim["oauth_consumer_key"]
+        oauth_consumer_key_sign = migration_claim["oauth_consumer_key_sign"]
+    except KeyError:
+        return False
+    base_string = "&".join(
+        [
+            oauth_consumer_key,
+            launch_data["https://purl.imsglobal.org/spec/lti/claim/deployment_id"],
+            launch_data["iss"],
+            launch_data["aud"],
+            str(launch_data["exp"]),
+            launch_data["nonce"],
+        ]
+    )
+    signed_base_string = hmac.digest(
+        oauth_secret.encode(), base_string.encode(), "sha256"
+    )
+    computed_signature = b64encode(signed_base_string).decode()
+    return oauth_consumer_key_sign == computed_signature
