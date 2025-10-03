26b19b8bf1a5d4586a7eaaf017dddc11fbba3a2b
Michael Wheeler <mpwheel@umich.edu>
2025-10-03 09:26:18 -0400

    Extract LTI 1.1 signature computation from validation

--- lti_tool/lti_core/utils.py
+++ lti_tool/lti_core/utils.py
@@ -10,6 +10,24 @@ def normalize_role(role: str) -> str:
     return role
 
 
+def compute_oauth_consumer_key_sign(
+    oauth_consumer_key: str,
+    deployment_id: str,
+    iss: str,
+    aud: str,
+    exp: int,
+    nonce: str,
+    oauth_secret: str,
+) -> str:
+    base_string = "&".join(
+        [oauth_consumer_key, deployment_id, iss, aud, str(exp), nonce]
+    )
+    signed_base_string = hmac.digest(
+        oauth_secret.encode(), base_string.encode(), "sha256"
+    )
+    return b64encode(signed_base_string).decode()
+
+
 def validate_migration_claim(launch_data: dict, oauth_secret: str) -> bool:
     """Verifies a signature passed in the LTI 1.1 migration claim."""
     try:
@@ -20,18 +38,13 @@ def validate_migration_claim(launch_data: dict, oauth_secret: str) -> bool:
         oauth_consumer_key_sign = migration_claim["oauth_consumer_key_sign"]
     except KeyError:
         return False
-    base_string = "&".join(
-        [
-            oauth_consumer_key,
-            launch_data["https://purl.imsglobal.org/spec/lti/claim/deployment_id"],
-            launch_data["iss"],
-            launch_data["aud"],
-            str(launch_data["exp"]),
-            launch_data["nonce"],
-        ]
+    computed_signature = compute_oauth_consumer_key_sign(
+        oauth_consumer_key,
+        launch_data["https://purl.imsglobal.org/spec/lti/claim/deployment_id"],
+        launch_data["iss"],
+        launch_data["aud"],
+        launch_data["exp"],
+        launch_data["nonce"],
+        oauth_secret,
     )
-    signed_base_string = hmac.digest(
-        oauth_secret.encode(), base_string.encode(), "sha256"
-    )
-    computed_signature = b64encode(signed_base_string).decode()
     return oauth_consumer_key_sign == computed_signature
