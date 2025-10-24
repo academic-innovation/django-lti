56b76d6c3ad068b8e3d54a96829cf6e0af8ebc79
Michael Wheeler <mpwheel@umich.edu>
2025-10-24 12:51:31 -0400

    Bump to v0.9.0

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ django.setup()
 project = "django-lti"
 copyright = "2024, Center for Academic Innovation"
 author = "Center for Academic Innovation"
-release = "0.8.1"
+release = "0.9.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
